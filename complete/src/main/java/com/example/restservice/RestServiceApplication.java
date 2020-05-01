edf45bc906de50a9c9857839572644cdbbbe9348
apple <apple@apples-MacBook-Pro-2.local>
2020-05-01 20:46:49 +0800

    mvn打包完成 启动方式java -jar target/xx.jar

--- complete/src/main/java/com/example/restservice/RestServiceApplication.java
+++ complete/src/main/java/com/example/restservice/RestServiceApplication.java
@@ -2,12 +2,21 @@ package com.example.restservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RestServiceApplication {
+public class RestServiceApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(RestServiceApplication.class, args);  //自动加载controller(用于接受用户请求) 和 service
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder builder
+    ){
+        return builder.sources(this.getClass());
     }
 
 }
